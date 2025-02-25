7f9b4f4decdacf9708f0ce1a439f9622294a8dbc
liaoyiqi <liaoyq@saichen.cn>
2025-02-25 09:50:28 +0800

    轮播图渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,24 @@
 <template>
   <view>
     <CustomNavbar />
+    <XtxSwiper :list="bannerList" />
   </view>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import type { BannerItem } from '@/types/home'
+import { getHomeBannerAPI } from '@/services/home'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const { result } = await getHomeBannerAPI()
+  bannerList.value = result
+}
+onLoad(async () => {
+  await Promise.allSettled([getHomeBannerData()])
+})
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/types/components.d.ts
@@ -0,0 +1,8 @@
+import type XtxSwiper from '@/components/XtxSwiper.vue'
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    XtxSwiper: typeof XtxSwiper
+  }
+}
+export type XtxSwiperInstance = InstanceType<typeof XtxSwiper>
